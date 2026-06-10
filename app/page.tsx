f34ac5cfc5bd00b25b01b48183bb716e43411814
abhishiv17 <abhishekshivanagoudar@gmail.com>
2026-06-11 01:37:42 +0530

    update: 4th semester

--- app/page.tsx
+++ app/page.tsx
@@ -213,7 +213,7 @@ export default function PortfolioPage() {
                         CGPA: 9.0
                       </span>
                       <span className="px-3 py-1.5 bg-zinc-100 dark:bg-zinc-800 rounded-lg text-xs font-bold text-zinc-600 dark:text-zinc-300">
-                        3rd Semester
+                        4th Semester
                       </span>
                     </div>
                   </div>
